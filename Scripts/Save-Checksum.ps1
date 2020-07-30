60007135536ccefc3697b2d94f0ae48dcaf3b816
Matt Gucci <matt9ucci@gmail.com>
2020-07-31 08:56:49 +0900

    Refactoring: Save-Checksum.ps1

--- Scripts/Save-Checksum.ps1
+++ Scripts/Save-Checksum.ps1
@@ -1,15 +1,25 @@
 param (
-	[string]$Version,
+	[string]
+	$Version,
+
 	[ValidateSet('MD5', 'SHA256')]
-	[string]$Algorithm = 'SHA256'
+	[string]
+	$Algorithm = 'SHA256'
 )
 
-Import-Module $PSScriptRoot\..\PSVBox\PSVBox -Force -Scope Local
+# Do not `ipmo PSVBox.psm1` because it throws an error if VirtualBox is not installed yet
+Import-Module $PSScriptRoot\..\PSVBox\Download.ps1 -Force -Scope Local
 
 if (!$Version) { $Version = Get-LatestVersion }
 $uri = "https://download.virtualbox.org/virtualbox/${Version}/${Algorithm}SUMS"
 $outFileDir = "$PSScriptRoot\..\Temp"
 $outFileName = Split-Path $uri -Leaf
 
-New-Item $outFileDir -ItemType Directory -Force > $null
-Invoke-WebRequest $uri -OutFile (Join-Path $outFileDir $outFileName) -Verbose
+New-Item $outFileDir -ItemType Directory -Force | Out-Null
+
+$param = @{
+	Uri = $uri
+	OutFile = Join-Path $outFileDir $outFileName
+	Verbose = $true
+}
+Invoke-WebRequest @param
